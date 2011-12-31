b1f954d8b454364e70b86d3234cf20e8a5973cb1
sharker <dblancoit@gmail.com>
2011-12-31 18:16:07 +0100

    Little CSS and translation fix. Added Creative Commons License from the original template

---
+++ apps/blog/blogpage.py
@@ -0,0 +1,170 @@
+#   Copyright 2011 Dario Blanco Iturriaga
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
+from datetime import date, datetime
+from blog.models import Entry
+from django.utils.translation import ugettext as _
+
+
+class BlogPage():
+    """ Refers to a page which lists 10 entries of the user's selected language.
+    """
+
+    def __init__(self, l):
+        self.language = l
+
+        
+    def __get_num_pages(self):
+        """ Returns the number of pages for a selected language (ej: 5 pages => 0-4).
+        """
+        nposts = Entry.objects.filter(lang = self.language, published = True).count()
+        return (nposts // 10) + 1
+
+
+    def get_nav_list(self, page):
+        """ Returns an adequate navigation list for a page.
+        """
+        page = int(page)
+        nav_list = []
+        npages = self.__get_num_pages()
+
+        if page < npages :
+            if page < 3 : # if page is in the first 5 elements
+                if npages > 4 :
+                    nav_list = [0,1,2,3,4]
+                elif npages == 4 :
+                    nav_list = [0,1,2,3]
+                elif npages == 3 :
+                    nav_list = [0,1,2]
+                elif npages == 2:
+                    nav_list = [0,1]
+                elif npages == 1:
+                    nav_list = [0]
+            elif page == (npages - 1) : # if page is the last page
+                nav_list = [page-4, page-3, page-2, page-1, page]
+            elif page == (npages - 2) : # if page is the second to last
+                nav_list = [page-3, page-2, page-1, page, page+1]
+            else : # if page is in the middle
+                nav_list = [page-2, page-1, page, page+1, page+2]
+            return nav_list
+        else :
+            return False
+
+
+    def posts_page(self, page):
+        """ Returns the list of entries of a single page (only in the user's selected language).
+        """
+        page = int(page)
+        
+        lower_bound = page * 10
+        upper_bound = (page + 1) * 10
+
+        # Shows entries with id from lower_bound to upper_bound-1 (including)
+        entries = Entry.objects.filter(lang = self.language,
+                                       published = True).order_by('-date')[lower_bound:upper_bound]
+
+        return entries
+
+
+    def posts_year(self, year) :
+        """ Returns the list of entries of a selected year.
+        """
+        return Entry.objects.filter(lang = self.language, published = True, date__year = year)
+
+
+    def posts_month(self, month, year) :
+        """ Returns the list of entries of a selected month and year.
+        """
+        return Entry.objects.filter(lang = self.language, published = True, date__year = year, date__month = month)
+
+
+    def archived_years(self):
+        """ Returns the list of archived years (years which have entries).
+        """
+        year_list = []
+
+        now = datetime.now()
+        actual_year = now.year
+        entry = Entry.objects.all().order_by('date')[0:1] # Getting the oldest entry
+        oldest_year = entry[0].date.year
+
+        y = oldest_year
+        object_year = Year(y, self.language)
+        year_list.append(object_year)
+        while y != actual_year :
+            y += 1
+            object_year = Year(y, self.language)
+            year_list.append(object_year)
+
+        return year_list
+
+    
+class Year():
+    """ Manages entries for a specific year and language.
+    """
+
+    def __init__(self, year_id, l):
+        self.months = []
+        self.language = l
+        self.year = year_id
+
+        # Calculate the month list
+        for m in range(1, 13) : # for each month from 1-12
+            if Entry.objects.filter(lang = self.language,
+                                    date__year = self.year,
+                                    date__month = m
+                                    ).count() > 0 :
+                month = Month()
+                month.year = year_id
+                month.language = l
+                month.month = m
+                self.months.append(month) # Adds the month number
+
+
+    def number_entries_year(self):
+        """ Return the total number of entries which belongs to this year.
+        """
+        return Entry.objects.filter(lang=self.language, date__year=self.year).count()
+
+
+    def __unicode__(self):
+        return str(self.year)
+
+
+    
+class Month():
+    """ Manage entries for a specific month, year and language.
+    """
+
+    def __init(self, m, y, l):
+        self.language = l
+        self.month = m
+        self.year = y
+
+
+    def number_entries_month(self):
+        """ Return the total number of entries which belongs to this year.
+        """
+        return Entry.objects.filter(lang = self.language,
+                                    date__year = self.year,
+                                    date__month = self.month).count()
+
+
+    def __unicode__(self):
+        """ Translates the month number to string.
+        """
+        d = date(self.year, self.month, 1)
+        mString = _(d.strftime("%B"))
+        # This string will be automatically translated to the desired language
+        return mString.capitalize()
---
+++ apps/blog/views.py
@@ -0,0 +1,375 @@
+#   Copyright 2011 Dario Blanco Iturriaga
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
+import datetime
+from django.http import HttpResponse, HttpResponseRedirect
+from django.template import RequestContext, loader
+from django.contrib.auth.models import User
+from django.contrib.auth import logout
+from blog.models import Entry, Link, Comment, EntryVotes, CommentVotes
+from blog.blogpage import BlogPage
+from blog.votes import Votes
+from blog.categorylister import CategoryLister
+from blog.forms import SubmitCommentForm
+from django.core.urlresolvers import reverse
+
+# TODO: generic views (https://docs.djangoproject.com/en/1.3/topics/class-based-views/)
+
+
+################
+# Own template generators
+################
+
+
+def __staticpage_template_gen(request, template_path):
+    """ Private function to generate the more simple templates in the blog.
+        Needed to accomplish the DRY principles.
+    """
+    cl = CategoryLister(request.LANGUAGE_CODE)
+
+    t = loader.get_template(template_path)
+
+    c = RequestContext(request, {
+        'cat_list' : cl.get_categories(),
+        'link_list' : Link.objects.all().order_by('order'),
+    })
+
+    return t.render(c)
+
+
+def __entrypage_template_gen(request, template_path, entry_list, nav_list, page_id):
+    """ Private function to generate templates related to entries list.
+        Needed to accomplish the DRY principles.
+    """
+    cl = CategoryLister(request.LANGUAGE_CODE)
+
+    t = loader.get_template(template_path)
+
+    c = RequestContext(request, {
+        'cat_list' : cl.get_categories(),
+        'blog_list': entry_list,
+        'nav_list' : nav_list,
+        'page' : int(page_id), # Very important the force check to int to avoid errors
+        'link_list' : Link.objects.all().order_by('order'),
+    })
+    
+    return t.render(c)
+
+
+def __singleentry_template_gen(request, template_path, entry, comment_list, form):
+    """ Private function to generate templates related to a single entry.
+        Needed to accomplish the DRY principles.
+    """
+    cl = CategoryLister(request.LANGUAGE_CODE)
+
+    t = loader.get_template(template_path)
+
+    template_datadict = {
+        'cat_list' : cl.get_categories(),
+        'entry': entry,
+        'link_list' : Link.objects.all().order_by('order'),
+        'comments' : comment_list,
+    }
+
+    if form is not None : # If form is needed, it adds it
+        template_datadict[form] = form
+
+    c = RequestContext(request, template_datadict)
+
+    return t.render(c)
+
+
+def __archivepage_template_gen(request, template_path, entry_list):
+    """ Private function to generate templates related to entries archives.
+        Needed to accomplish the DRY principles.
+    """
+    cl = CategoryLister(request.LANGUAGE_CODE)
+
+    t = loader.get_template(template_path)
+
+    c = RequestContext(request, {
+        'cat_list'  : cl.get_categories(),                  # Alphabetic order
+        'blog_list' : entry_list,                           # Entry list
+        'link_list' : Link.objects.all().order_by('order'), # Priority order
+    })
+
+    return t.render(c)
+
+
+
+################
+# Views
+################
+
+
+def blog_entries_view(request, page_id):
+    """ Entries list by page (0 is the first and more recent page).
+    """
+    bp = BlogPage(request.LANGUAGE_CODE)
+    return HttpResponse(__entrypage_template_gen(request,
+                                                 'blog/blog_view.html',
+                                                 bp.posts_page(page_id),
+                                                 bp.get_nav_list(page_id),
+                                                 page_id))
+
+
+def single_entry_view(request, post_id):
+    """ Single entry details: entire text and comments.
+    """
+    return HttpResponse(__singleentry_template_gen(request,
+                                                   'blog/blog_singlepost.html',
+                                                   Entry.objects.get(id = post_id),
+                                                   Comment.objects.filter(entry = post_id),
+                                                   None))
+
+
+def single_category_view(request, cat_id):
+    """ Entries list of a single category.
+    """
+    bp = BlogPage(request.LANGUAGE_CODE)
+    return HttpResponse(__entrypage_template_gen(request,
+                                                 'blog/blog_view.html',
+                                                 Entry.objects.filter(cat__id = cat_id),
+                                                 bp.get_nav_list(0),
+                                                 0))
+
+
+def blog_category_view(request):
+    """ The entire blog category list with its description per category.
+    """
+    return HttpResponse(__staticpage_template_gen(request, 'blog/category_view.html'))
+
+
+def aboutme_view(request):
+    """ About me static page.
+    """
+    return HttpResponse(__staticpage_template_gen(request, 'blog/aboutme_view.html'))
+
+
+def privacy_view(request):
+    """ Static page which expands the privacy policy
+    """
+    return HttpResponse(__staticpage_template_gen(request, 'blog/privacy_view.html'))
+
+
+def logout_request(request):
+    """ Login out with an user. When completed, redirects to the index page (blog list page 0).
+    """
+    logout(request)
+    return HttpResponseRedirect(reverse('index'))
+
+
+def archive_view(request) :
+    """ List of year and months with the number of entries related.
+    """
+    bp = BlogPage(request.LANGUAGE_CODE)
+    return HttpResponse(__archivepage_template_gen(request, 'blog/archives_view.html', bp.archived_years()))
+
+
+def archive_year_request(request, year_id) :
+    """ List of entries related to a specific year.
+    """
+    bp = BlogPage(request.LANGUAGE_CODE)
+    return HttpResponse(__archivepage_template_gen(request, 'blog/blog_view.html', bp.posts_year(year_id)))
+
+
+def archive_month_request(request, month_id, year_id) :
+    """ List of entries related to a specific year and month.
+    """
+    bp = BlogPage(request.LANGUAGE_CODE)
+    return HttpResponse(__archivepage_template_gen(request, 'blog/blog_view.html', bp.posts_month(month_id, year_id)))
+
+
+def comment_entry_request(request, post_id, user_id):
+    """ Triggered when a user inserts a comment in an specific entry.
+    """
+    bp = Entry.objects.get(id = post_id)
+    # Form management
+    if request.method == 'POST': # If the form has been submitted...
+        form = SubmitCommentForm(request.POST) # A form bound to the POST data
+        if form.is_valid(): # All validation rules pass
+            comment = Comment(entry = bp,
+                              author = User.objects.get(id = user_id),
+                              text = request.POST['message'],
+                              date = datetime.datetime.now(),
+                              num = Comment.objects.filter(entry = post_id).count() + 1,
+                              quote = 0)
+
+            comment.save()
+
+            return HttpResponseRedirect(reverse("singlepost", args=(post_id,))) # Redirect after POST
+    else:
+        form = SubmitCommentForm() # An unbound form
+
+    return HttpResponse(__singleentry_template_gen(request,
+                                                   'blog/blog_singlepost.html',
+                                                   Entry.objects.get(id = post_id),
+                                                   Comment.objects.filter(entry = post_id),
+                                                   form))
+
+
+def rate_entry_request(request, post_id, user_id):
+    """ Triggered when a user rates a specific entry.
+    """
+    entry = Entry.objects.get(id = post_id)
+    user = User.objects.get(id = user_id)
+    positive = True
+
+    if request.POST['like']:
+        positive = True
+    else :
+        positive = False
+
+    v = Votes()
+    if v.validate_entry_vote(user_id, post_id):
+        vote = EntryVotes(entry = entry,
+                          user = user,
+                          positive = positive)
+        vote.save()
+        v.set_entry_rate(post_id)
+
+    return HttpResponseRedirect(reverse("singlepost", args=(post_id,))) # Redirect after POST
+
+
+def rate_comment_request(request, post_id, comment_id, user_id):
+    """ Triggered when a user rates a specific comment from a specific entry.
+    """
+    comment = Comment.objects.get(id = comment_id)
+    user = User.objects.get(id = user_id)
+    positive = True
+
+    if request.POST['like']:
+        positive = True
+    else:
+        positive = False
+
+    v = Votes()
+    if v.validate_comment_vote(user_id, comment_id) :
+        vote = CommentVotes(comment = comment,
+                            user = user,
+                            positive = positive)
+        vote.save()
+        v.set_comment_rate(comment_id)
+
+    return HttpResponseRedirect(reverse("singlepost", args=(post_id,))) # Redirect after POST
+
+
+
+#############################
+# OTHER VIEWS THAT COULD HAVE BEEN USEFUL BUT I DECIDED NOT OR ARE TODO VIEWS
+#############################
+
+#def search_request(request):
+#    keywords = request.POST['keywords']
+#
+#    bp = BlogPage(request.LANGUAGE_CODE)
+#    cl = CategoryLister(request.LANGUAGE_CODE)
+#
+#    t = loader.get_template('blog/blog_view.html')
+#
+#    c = RequestContext(request, {
+#        'cat_list' : cl.getCategories(),
+#        'blog_list': bp.postsSearch(keywords),
+#        'nav_list' : bp.getNavList(0),
+#        'page' : 0,
+#        'link_list' : Link.objects.all().order_by('order'),
+#    })
+#
+#    return HttpResponse(t.render(c))
+
+## Static page with a login request
+#def login_view(request):
+#    cl = CategoryLister(request.LANGUAGE_CODE)
+#
+#    t = loader.get_template('blog/login_view.html')
+#
+#    username = request.POST['username']
+#    password = request.POST['password']
+#    user = authenticate(username=username, password=password)
+#
+#    usererror = False
+#    if user is not None:
+#        if user.is_active:
+#            login(request, user)
+#            # Redirect to a success page
+#            message = _('Login successful')
+#        else:
+#            # Return a disabled account message
+#            usererror = True
+#            message = _('The account is disabled ')
+#    else :
+#        # Return an invalid login error message
+#        usererror = True
+#        message = _('Invalid username and/or password')
+#
+#
+#    c = RequestContext(request, {
+#        'cat_list' : cl.getCategories(),
+#        'link_list' : Link.objects.all().order_by('order'),
+#        'usererror' : usererror,
+#        'username' : username,
+#        'message' : message,
+#    })
+#
+#    return HttpResponse(t.render(c))
+
+
+## Static page with a register request
+#def register_view(request):
+#    cl = CategoryLister(request.LANGUAGE_CODE)
+#
+#    t = loader.get_template('blog/register_view.html')
+#
+#    c = RequestContext(request, {
+#        'cat_list' : cl.getCategories(),
+#        'link_list' : Link.objects.all().order_by('order'),
+#        'reg_pending' : False,
+#    })
+#
+#    return HttpResponse(t.render(c))
+
+#def register_request(request):
+#    cl = CategoryLister(request.LANGUAGE_CODE)
+#
+#    user = User.objects.create_user(request.POST['username'],
+#                                    request.POST['email'],
+#                                    request.POST['password'])
+#
+#    user.save()
+#
+#    t = loader.get_template('blog/register_view.html')
+#
+#    c = RequestContext(request, {
+#        'cat_list' : cl.getCategories(),
+#        'link_list' : Link.objects.all().order_by('order'),
+#        'reg_pending' : True,
+#    })
+#
+#    return HttpResponse(t.render(c))
+
+# Login complete view, displays user data
+#@login_required
+#def done(request):
+#    cl = CategoryLister(request.LANGUAGE_CODE)
+#
+#    t = loader.get_template('blog/login_view.html')
+#
+#    c = RequestContext(request, {
+#        'cat_list' : cl.getCategories(),
+#        'link_list' : Link.objects.all().order_by('order'),
+#        'version': version,
+#        'last_login': request.session.get('social_auth_last_login_backend')
+#    })
+#
+#    return HttpResponse(t.render(c))
